9f3b65de5c601cd42fa79a6f81036f52d6b01597
yyl20050115 <yyl_20050115@hotmail.com>
2023-02-28 21:50:33 +0800

    fx

--- Src/Fare/Xeger.cs
+++ Src/Fare/Xeger.cs
@@ -20,124 +20,118 @@
 using System;
 using System.Text;
 
-namespace Fare
+namespace Fare;
+
+/// <summary>
+/// An object that will generate text from a regular expression. In a way, 
+/// it's the opposite of a regular expression matcher: an instance of this class
+/// will produce text that is guaranteed to match the regular expression passed in.
+/// </summary>
+public class Xeger
 {
+    private const RegExpSyntaxOptions AllExceptAnyString 
+        = RegExpSyntaxOptions.All & ~RegExpSyntaxOptions.Anystring;
+
+    private readonly Automaton automaton;
+    private readonly Random random;
+
     /// <summary>
-    /// An object that will generate text from a regular expression. In a way, 
-    /// it's the opposite of a regular expression matcher: an instance of this class
-    /// will produce text that is guaranteed to match the regular expression passed in.
+    /// Initializes a new instance of the <see cref="Xeger"/> class.
     /// </summary>
-    public class Xeger
+    /// <param name="regex">The regex.</param>
+    /// <param name="random">The random.</param>
+    public Xeger(string regex, Random random)
     {
-        private const RegExpSyntaxOptions AllExceptAnyString = RegExpSyntaxOptions.All & ~RegExpSyntaxOptions.Anystring;
-
-        private readonly Automaton automaton;
-        private readonly Random random;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Xeger"/> class.
-        /// </summary>
-        /// <param name="regex">The regex.</param>
-        /// <param name="random">The random.</param>
-        public Xeger(string regex, Random random)
+        if (string.IsNullOrEmpty(regex))
         {
-            if (string.IsNullOrEmpty(regex))
+            throw new ArgumentNullException(nameof(regex));
+        }
+
+        regex = RemoveStartEndMarkers(regex);
+        this.automaton = new RegExp(regex, AllExceptAnyString).ToAutomaton();
+        this.random = random ?? throw new ArgumentNullException(nameof(random));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Xeger"/> class.<br/>
+    /// Note that if multiple instances are created within short time using this overload,<br/>
+    /// the instances might generate identical random strings.<br/>
+    /// To avoid this, use the constructor overload that accepts an argument of type Random.
+    /// </summary>
+    /// <param name="regex">The regex.</param>
+    public Xeger(string regex)
+        : this(regex, new Random())
+    {
+    }
+
+    /// <summary>
+    /// Generates a random String that is guaranteed to match the regular expression passed to the constructor.
+    /// </summary>
+    /// <returns></returns>
+    public string Generate()
+    {
+        var builder = new StringBuilder();
+        this.Generate(builder, this.automaton.Initial);
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Generates a random number within the given bounds.
+    /// </summary>
+    /// <param name="min">The minimum number (inclusive).</param>
+    /// <param name="max">The maximum number (inclusive).</param>
+    /// <param name="random">The object used as the randomizer.</param>
+    /// <returns>A random number in the given range.</returns>
+    private static int GetRandomInt(int min, int max, Random random)
+    {
+        int maxForRandom = max - min + 1;
+        return random.Next(maxForRandom) + min;
+    }
+
+    private void Generate(StringBuilder builder, State state)
+    {
+        var transitions = state.GetSortedTransitions(true);
+        if (transitions.Count == 0)
+        {
+            if (!state.Accept)
             {
-                throw new ArgumentNullException("regex");
+                throw new InvalidOperationException("state");
             }
 
-            if (random == null)
-            {
-                throw new ArgumentNullException("random");
-            }
-
-
-            regex = RemoveStartEndMarkers(regex);
-            this.automaton = new RegExp(regex, AllExceptAnyString).ToAutomaton();
-            this.random = random;
+            return;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Xeger"/> class.<br/>
-        /// Note that if multiple instances are created within short time using this overload,<br/>
-        /// the instances might generate identical random strings.<br/>
-        /// To avoid this, use the constructor overload that accepts an argument of type Random.
-        /// </summary>
-        /// <param name="regex">The regex.</param>
-        public Xeger(string regex)
-            : this(regex, new Random())
+        int nroptions = state.Accept ? transitions.Count : transitions.Count - 1;
+        int option = Xeger.GetRandomInt(0, nroptions, this.random);
+        if (state.Accept && option == 0)
         {
+            // 0 is considered stop.
+            return;
         }
 
-        /// <summary>
-        /// Generates a random String that is guaranteed to match the regular expression passed to the constructor.
-        /// </summary>
-        /// <returns></returns>
-        public string Generate()
+        // Moving on to next transition.
+        Transition transition = transitions[option - (state.Accept ? 1 : 0)];
+        this.AppendChoice(builder, transition);
+        Generate(builder, transition.To);
+    }
+
+    private void AppendChoice(StringBuilder builder, Transition transition)
+    {
+        var c = (char)Xeger.GetRandomInt(transition.Min, transition.Max, this.random);
+        builder.Append(c);
+    }
+
+    private static string RemoveStartEndMarkers(string regExp)
+    {
+        if (regExp.StartsWith("^", StringComparison.Ordinal))
         {
-            var builder = new StringBuilder();
-            this.Generate(builder, automaton.Initial);
-            return builder.ToString();
+            regExp = regExp[1..];
         }
-
-        /// <summary>
-        /// Generates a random number within the given bounds.
-        /// </summary>
-        /// <param name="min">The minimum number (inclusive).</param>
-        /// <param name="max">The maximum number (inclusive).</param>
-        /// <param name="random">The object used as the randomizer.</param>
-        /// <returns>A random number in the given range.</returns>
-        private static int GetRandomInt(int min, int max, Random random)
+        if (regExp.EndsWith("$", StringComparison.Ordinal))
         {
-            int maxForRandom = max - min + 1;
-            return random.Next(maxForRandom) + min;
+            regExp = regExp[..^1];
         }
 
-        private void Generate(StringBuilder builder, State state)
-        {
-            var transitions = state.GetSortedTransitions(true);
-            if (transitions.Count == 0)
-            {
-                if (!state.Accept)
-                {
-                    throw new InvalidOperationException("state");
-                }
-
-                return;
-            }
-
-            int nroptions = state.Accept ? transitions.Count : transitions.Count - 1;
-            int option = Xeger.GetRandomInt(0, nroptions, random);
-            if (state.Accept && option == 0)
-            {
-                // 0 is considered stop.
-                return;
-            }
-
-            // Moving on to next transition.
-            Transition transition = transitions[option - (state.Accept ? 1 : 0)];
-            this.AppendChoice(builder, transition);
-            Generate(builder, transition.To);
-        }
-
-        private void AppendChoice(StringBuilder builder, Transition transition)
-        {
-            var c = (char)Xeger.GetRandomInt(transition.Min, transition.Max, random);
-            builder.Append(c);
-        }
-
-        private string RemoveStartEndMarkers(string regExp)
-        {
-            if (regExp.StartsWith("^"))
-            {
-                regExp = regExp[1..];
-            }
-            if (regExp.EndsWith("$"))
-            {
-                regExp = regExp[..^1];
-            }
-
-            return regExp;
-        }
+        return regExp;
     }
 }
